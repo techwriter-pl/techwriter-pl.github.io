eb9fa49a27f95d7ec60dfe68b269eb1038d8f6d1
Paweł Kowaluk <pensjonatus@gmail.com>
2023-08-29 23:03:37 +0200

    fix Disqus

---
+++ src/components/DisqusEmbed.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { DiscussionEmbed } from "disqus-react";
+
+type DisqusEmbedProps = {
+  relativeUrl: string;
+  id: string;
+  title: string;
+};
+
+export default function DisqusEmbed({
+  relativeUrl,
+  id,
+  title,
+}: DisqusEmbedProps) {
+  const { siteConfig } = useDocusaurusContext();
+  const { url } = siteConfig;
+
+  return (
+    <div className="margin-top--lg margin-bottom--lg">
+      <DiscussionEmbed
+        shortname="techwriter-pl"
+        config={{
+          url: `${url}${relativeUrl}`,
+          identifier: id,
+          title: title,
+          language: "pl",
+        }}
+      />
+    </div>
+  );
+}
--- src/theme/BlogPostItem/Footer.tsx
+++ src/theme/BlogPostItem/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import OriginalBlogPostFooter from "@theme-original/BlogPostItem/Footer";
-import { DiscussionEmbed } from "disqus-react";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
+import DisqusEmbed from "@site/src/components/DisqusEmbed";
 
 export default function Footer() {
   const { metadata } = useBlogPost();
@@ -9,15 +9,7 @@ export default function Footer() {
   return (
     <>
       <OriginalBlogPostFooter />
-      <DiscussionEmbed
-        shortname="techwriter-pl"
-        config={{
-          url: permalink,
-          identifier: date,
-          title: title,
-          language: "pl_PL",
-        }}
-      />
+      <DisqusEmbed title={title} id={date} relativeUrl={permalink} />
     </>
   );
 }
