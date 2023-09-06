01bb7a24dde51f2e3164345f5566f249b78656a8
Paweł Kowaluk <pensjonatus@gmail.com>
2023-09-06 20:47:09 +0200

    display contents in post preview

--- src/components/BlogList/BlogList.tsx
+++ src/components/BlogList/BlogList.tsx
@@ -11,44 +11,56 @@ import clsx from "clsx";
 export default function BlogHome(props): JSX.Element {
   const { pathname, search } = props.history.location;
   const { siteConfig } = useDocusaurusContext();
-  console.log(props)
 
   if (pathname.includes("/page/") || search.includes("feed=true")) {
     return <BlogListPage {...props} />;
   }
 
   return (
-    <Layout
-      title={siteConfig.title}
-      description={siteConfig.tagline}
-      wrapperClassName={styles.pageWrapper}
-    >
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <HomePageHeader />
       <div className={styles.topWrapper}>
         <div className="container">
           <h2 className={styles.articleListTitle}>Najnowsze artykuły</h2>
           <div className={styles.items}>
-            {props.items.slice(0, 3).map(({ content }) => {
-              const { date, permalink } = content.metadata;
-              const { title, coverImage, tags } = content.frontMatter;
+            {props.items.slice(0, 3).map(({ content: BlogPostContent }) => {
+              const { date, permalink, authors } = BlogPostContent.metadata;
+              const { title, coverImage, tags } = BlogPostContent.frontMatter;
               const coverImagePath = `/img/cover/${coverImage}`;
               return (
-                <Link
-                  to={permalink}
-                  key={date}
-                  className={clsx(styles.item, "card")}
-                >
+                <div key={date} className={clsx(styles.item, "card")}>
                   <div className={styles.itemLeft}>
-                    <div className={styles.itemTitle}>{title}</div>
-                    <div>{new Date(date).toLocaleDateString('pl', { year:"numeric", month:"short", day:"numeric"})}</div>
-                    <div className={styles.tagList}>{tags.map((tag, tagKey) => <span key={tagKey} className="badge badge--secondary">{tag}</span>)}</div>
+                    <Link to={permalink} className={styles.itemTitle}>
+                      {title}
+                    </Link>
+                    <div>
+                      {new Date(date).toLocaleDateString("pl", {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                      , {authors.map((author) => author.name).join(", ")}
+                    </div>
+                    <div className={styles.tagList}>
+                      {tags.map((tag, tagKey) => (
+                        <span key={tagKey} className="badge badge--secondary">
+                          {tag}
+                        </span>
+                      ))}
+                    </div>
                   </div>
-                  <img
-                    alt=""
-                    src={coverImagePath}
-                    className={styles.itemImage}
-                  />
-                </Link>
+                  <div>
+                    <BlogPostContent />{" "}
+                    <Link to={permalink}>czytaj więcej...</Link>
+                  </div>
+                  <Link to={permalink} className={styles.imageContainer}>
+                    <img
+                      alt=""
+                      src={coverImagePath}
+                      className={styles.itemImage}
+                    />
+                  </Link>
+                </div>
               );
             })}
           </div>
