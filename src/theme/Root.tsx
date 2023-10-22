0dfd9c4e905279914c81592a8bf782acc9eb9160
Paweł Kowaluk <pensjonatus@gmail.com>
2023-10-22 10:38:26 +0200

    add a cookie consent form

---
+++ src/theme/Root.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import CookieDialog from '../components/CookieDialog/CookieDialog';
+import { initializeGtag, setDefaultConsent } from '../lib/gtag';
+
+export default function Root({ children }) {
+  useEffect(() => {
+    initializeGtag();
+    setDefaultConsent();
+  }, []);
+
+  return (
+    <>
+      {children}
+      <CookieDialog />
+    </>
+  );
+}
