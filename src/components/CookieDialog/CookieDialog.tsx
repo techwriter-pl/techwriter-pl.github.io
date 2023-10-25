129e4155f14020b9da4a77bfa0c423c2e036d22e
Pawel Kowaluk <pensjonatus@gmail.com>
2023-10-25 18:15:59 +0000

    dodaj cudzysłów

--- src/components/CookieDialog/CookieDialog.tsx
+++ src/components/CookieDialog/CookieDialog.tsx
@@ -21,8 +21,8 @@ export default function CookieDialog() {
     <div className={styles.card}>
       <div className={styles.title}>Ta strona używa "cookies" 🍪</div>
       <p>
-        Cookies czyli ciasteczka to pliki umożliwiające zapisywanie informacji o
-        Tobie i Twoich działaniach w Internecie. Nasza strona używa analityki
+        Cookies czyli "ciasteczka" to pliki umożliwiające zapisywanie informacji
+        o Tobie i Twoich działaniach w Internecie. Nasza strona używa analityki
         Google'a więc Google dokłada do niej swoje pliki cookies, żeby
         realizować swoje cele biznesowe.
       </p>
