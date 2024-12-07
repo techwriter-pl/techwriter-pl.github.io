5f3b5916b9e07d62f75806d5506d45d60a8866d0
Pawel Kowaluk <pensjonatus@gmail.com>
2024-12-07 15:32:33 +0000

    Jeszcze lepszy wygląd cover imedża

--- src/theme/BlogPostItem/Header/Title.tsx
+++ src/theme/BlogPostItem/Header/Title.tsx
@@ -18,9 +18,8 @@ export default function Footer() {
       <Link href={postLink}>
         <div
           style={{
-            // backgroundColor: 'var(--ifm-color-primary)',
             backgroundColor: 'black',
-            padding: '1rem',
+            padding: '4px 1rem',
             display: 'flex',
             justifyContent: 'center',
             marginBottom: '1rem',
