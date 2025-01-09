9ba411c6f290088645f0486972f58beb3e4df029
Michał Skowron <39494509+docdeveloper@users.noreply.github.com>
2025-01-09 18:08:44 +0100

    Update job-offer-list.tsx

--- src/components/JobOffers/job-offer-list.tsx
+++ src/components/JobOffers/job-offer-list.tsx
@@ -14,12 +14,13 @@ export const jobOffers: JobOfferProps[] = [
     link: 'https://techland.net/job-offers/technical-writer-15',
     location: 'Warszawa, Wrocław',
     type: 'w biurze',
-    description: `Techland to firma z wieloletnią historią w branży gier.
-    Dzięki ponad 30-letniemu doświadczeniu na rynku wie, jak dostarczać niezapomniane wrażenia. 
-    Ich międzynarodowy zespół z siedzibą w Polsce składa się z ponad 500 profesjonalistów.
-    Są znani z takich gier jak The Call of Juarez i Dying Light. Ale na tym nie poprzestają. Chcą nadal tworzyć fascynujące tytuły AAA oparte na nowatorskiej rozgrywce.
-    
-    Obecnie Techland poszukuje osoby na stanowisko Technical Writera, która będzie odpowiedzialna za pisanie i ulepszanie dokumentacji technicznej dla twórców treści i deweloperów.`,
+    description: `Techland to firma z wieloletnią historią w branży gier. 
+Dzięki ponad 30-letniemu doświadczeniu na rynku wie, jak dostarczać niezapomniane wrażenia.
+Ich międzynarodowy zespół z siedzibą w Polsce składa się z ponad 500 profesjonalistów. 
+Są znani z takich gier jak The Call of Juarez i Dying Light. Ale na tym nie poprzestają. Chcą nadal tworzyć fascynujące tytuły AAA oparte na nowatorskiej rozgrywce.
+
+Obecnie Techland poszukuje osoby na stanowisko Technical Writera, 
+która będzie odpowiedzialna za pisanie i ulepszanie dokumentacji technicznej dla twórców treści i deweloperów.`,
   },
   {
     title: 'Technical Writer',
