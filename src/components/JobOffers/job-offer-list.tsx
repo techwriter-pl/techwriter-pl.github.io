6938fe187231adc3e319d09bd14d9ded8b965e80
monBon-Ton <119131560+monBon-Ton@users.noreply.github.com>
2025-02-12 18:47:53 +0000

    oferta pracy splunk

--- src/components/JobOffers/job-offer-list.tsx
+++ src/components/JobOffers/job-offer-list.tsx
@@ -18,6 +18,21 @@ export const jobOffers: JobOfferProps[] = [
       Nie jest to oferta płatnej pracy, ale może dać Ci dużo innych korzyści.`,
   },
   {
+    title: 'Senior Technical Writer - Developer Content',
+    company: 'Splunk',
+    link: 'https://www.splunk.com/en_us/careers/jobs/senior-technical-writer-developer-31867.html',
+    location: 'Kraków',
+    type: 'hybrydowo',
+    description: `Splunk tworzy oprogramowanie do monitorowania i analizy dużej ilości danych w czasie rzeczywistym. 
+  Splunk również skrupulatnie buduje swoją obecność w Krakowie intensywnie rekrutując. 
+
+  Kogo szukają? 
+
+  Jeśli jesteś doświadczonym Technical Writerem, ale zawsze ciągnęło Cię w stronę kodu, jeśli do tego dobrze rozumiesz się z programistami i udzielasz się w projektach open sourceowych - jest duża szansa, że szukasz właśnie takiej oferty!
+  
+  P.S. Nie będziesz jedynym Tech Writerem na miejscu. I choć większość zespołu dokumentalistów znajduje się w Stanach, w Polsce mamy już 5 pisarzy, którzy wspierają lokalne zespoły developerskie.`,
+  },
+  {
     title: 'Technical Writer',
     company: 'Techland',
     link: 'https://techland.net/job-offers/technical-writer-15',
