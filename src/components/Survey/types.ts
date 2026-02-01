21e15bbc823a88188da42234f8cbd2f0a67a1acb
Paweł Kowaluk <pensjonatus@gmail.com>
2026-02-01 08:31:36 +0100

    Dodaj wyniki badania płac 2025

--- src/components/Survey/types.ts
+++ src/components/Survey/types.ts
@@ -28,11 +28,13 @@ export type Question =
   | 'Płeć'
   | 'Jak oceniasz zadowolenie ze swojej pracy w skali od 1 do 5?'
   | 'W roku 2024, przez ile miesięcy dotknęło Cię bezrobocie?'
-  | 'Czy w roku 2024 Twoje całkowite wynagrodzenie brutto się zmieniło?';
+  | 'W roku 2025, przez ile miesięcy dotknęło Cię bezrobocie?'
+  | 'Czy w roku 2024 Twoje całkowite wynagrodzenie brutto się zmieniło?'
+  | 'Czy w roku 2025 Twoje całkowite wynagrodzenie brutto wzrosło, zmalało, cze pozostało na tym samym poziomie?';
 
 export type NumberOfPeopleAndEarnings = {
   [responseLabel: string]: string;
-  mediana: string;
-  minimalne: string;
-  maksymalne: string;
+  Mediana: string;
+  Minimalne: string;
+  Maksymalne: string;
 };
